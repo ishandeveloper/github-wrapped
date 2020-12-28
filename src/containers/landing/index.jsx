5c5c3bccbd334385e616aac51e6435eb2a65a8c6
kothariji <kotharidhruv25@gmail.com>
2020-12-29 00:47:15 +0530

    changes made

--- src/containers/landing/index.jsx
+++ src/containers/landing/index.jsx
@@ -11,16 +11,28 @@ import Particles from "react-particles-js";
 import { TweenMax } from "gsap";
 import { LandingLoader, LandingHeader } from "../../components";
 import { useHistory } from "react-router-dom";
+import {Snackbar} from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+
 
 export default function LandingPage() {
   const [username, setUsername] = useState("");
+  const [inValidUsername, setInValidUsername] = useState(false);
   const octocatRef = useRef();
   const landingRef = useRef();
   const loaderRef = useRef();
   const history = useHistory();
 
+  const verifyUsername = (test_username) => {
+    let github_username_regex = new RegExp("^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$");
+    return github_username_regex.test(test_username);
+  }
+
   const handleUsernameSubmit = (e) => {
-    if (e.key === "Enter" && username !== "") {
+    if (e.key === "Enter" && username !== "" && verifyUsername(username)) {
+      
+      setInValidUsername(false);
+
       // GSAP ANIMATIONS
       TweenMax.to(octocatRef.current, 0.5, {
         opacity: 0,
@@ -35,6 +47,9 @@ export default function LandingPage() {
       TweenMax.to(loaderRef.current, 1, { opacity: 1 });
 
       setTimeout(() => history.push(`/${username}`), 2000);
+    }
+    else if(e.key === "Enter"){
+      setInValidUsername(true);
     }
   };
 
@@ -66,6 +81,11 @@ export default function LandingPage() {
       <div className="landing__loader" ref={loaderRef}>
         <LandingLoader />
       </div>
+      <Snackbar open={inValidUsername} autoHideDuration={300}>
+        <MuiAlert elevation={6} severity="error" variant="filled">
+          Please enter a valid github Username. 
+        </MuiAlert>
+      </Snackbar>
     </section>
   );
 }
