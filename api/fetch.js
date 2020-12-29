e6b20a3608f8105a9d50e18f2afa515f901ed28f
ishandeveloper <ishandeveloper@outlook.com>
2020-12-29 20:55:17 +0530

    Added Data Filtering for all stats

--- api/fetch.js
+++ api/fetch.js
@@ -1,37 +1,18 @@
 const axios = require("axios");
 require("dotenv").config();
 
-function request(data, headers) {
+// Github Search REST API currently does not support sorting repositories by number of stars
+const fetchTotalStars = (parameters, token) => {
   return axios({
     url: "https://api.github.com/graphql",
     method: "post",
-    headers,
-    data,
-  });
-}
-
-const graphfetch = (parameters, token) => {
-  return request(
-    {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+    data: {
       query: `
         query userInfo($login: String!) {
           user(login: $login) {
-            name
-            login
-            contributionsCollection {
-              totalCommitContributions
-              restrictedContributionsCount
-            }
-            repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {
-              totalCount
-            }
-            pullRequests(first: 1) {
-              totalCount
-            }
-            issues(first: 1) {
-              totalCount
-            }
-
             repositories(first: 100, ownerAffiliations: OWNER, orderBy: {direction: DESC, field: STARGAZERS}) {
               totalCount
               nodes {
@@ -43,16 +24,82 @@ const graphfetch = (parameters, token) => {
           }
         }
         `,
-
       variables: parameters,
     },
-    {
-      Authorization: `bearer ${token}`,
-    }
+  }).then((e) =>
+    e.data.data.user.repositories.nodes.reduce((prev, curr) => {
+      return prev + curr.stargazers.totalCount;
+    }, 0)
   );
 };
-export async function fetchStats(username) {
-  let res = await graphfetch({ login: username }, process.env.TOKEN);
 
-  return res.data;
+// https://docs.github.com/en/free-pro-team@latest/rest/reference/search#search-commits
+const fetchTotalCommits = (variables, token) => {
+  return axios({
+    method: "get",
+    url: `https://api.github.com/search/commits?q=author:${variables.login}+committer-date:>2020-01-01`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.cloak-preview",
+      Authorization: `bearer ${token}`,
+    },
+  }).then((e) => e.data.total_count);
+};
+
+//https://docs.github.com/en/free-pro-team@latest/rest/reference/search#search-issues-and-pull-requests
+const fetchTotalIssues = (params, token) => {
+  return axios({
+    method: "get",
+    url: `https://api.github.com/search/issues?q=author:${params.login}+is:issue+created:2020-01-01..2020-12-31`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.cloak-preview",
+      Authorization: `bearer ${token}`,
+    },
+  }).then((e) => e.data.total_count);
+};
+
+//https://docs.github.com/en/free-pro-team@latest/rest/reference/search#search-issues-and-pull-requests
+const fetchTotalPRs = (params, token) => {
+  return axios({
+    method: "get",
+    url: `https://api.github.com/search/issues?q=author:${params.login}+is:pr+created:2020-01-01..2020-12-31`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.cloak-preview",
+      Authorization: `bearer ${token}`,
+    },
+  }).then((e) => e.data.total_count);
+};
+
+const statsFetch = async (parameters) => {
+  const _stats = {
+    pr: 0,
+    stars: 0,
+    issues: 0,
+    commits: 0,
+  };
+
+  _stats.commits = await fetcher(fetchTotalCommits, parameters);
+  _stats.issues = await fetcher(fetchTotalIssues, parameters);
+  _stats.pr = await fetcher(fetchTotalPRs, parameters);
+  _stats.stars = await fetcher(fetchTotalStars, parameters);
+
+  return _stats;
+};
+
+// Resolves the relevant function with tokens
+const fetcher = async (func, params) => {
+  try {
+    let res = await func(params, process.env.TOKEN);
+    return res;
+  } catch (e) {
+    console.log(e);
+    return 0;
+  }
+};
+
+export async function fetchStats(username) {
+  let res = await statsFetch({ login: username });
+  return res;
 }
