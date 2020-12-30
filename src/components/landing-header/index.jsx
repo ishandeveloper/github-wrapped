b28265dcf31a6b4a7763950798fcba94a847bfaa
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-12-30 12:14:14 +0200

    Fix Github -> GitHub

--- src/components/landing-header/index.jsx
+++ src/components/landing-header/index.jsx
@@ -24,7 +24,7 @@ export default function LandingHeader({
       <input
         className="landing__input"
         type="text"
-        placeholder="Your Github Username"
+        placeholder="Your GitHub Username"
         onChange={inputHandler}
         value={username}
         onKeyUp={keyUpHandler}
